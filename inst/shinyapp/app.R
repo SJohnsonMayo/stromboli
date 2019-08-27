a425a964a2506f80326798d0319ed3e64c733d49
Johnson-Stephen <Johnson.Stephen1@mayo.edu>
2019-08-27 18:11:36 -0500

    Exporting subset_dist. Adding Shiny app -- needs more testing

--- inst/shinyapp/app.R
+++ inst/shinyapp/app.R
@@ -1,14 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-#
-#
-# Adding more comments -- test
+
 
 library(shiny)
 library(shinydashboard)
